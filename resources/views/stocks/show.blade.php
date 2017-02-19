74e6e8dc54e532728bbcb2c9583731672764da04
Caitlin Rooney <Caitlin Rooney>
2017-02-19 15:03:45 -0600

    added current stocks to read page

--- resources/views/stocks/show.blade.php
+++ resources/views/stocks/show.blade.php
@@ -25,6 +25,43 @@
                 <td>Date Purchased</td>
                 <td><?php echo ($stock['purchased']); ?></td>
             </tr>
+            <tr>
+                <td>Current Price</td>
+                <td>
+                    <?php
+                    $ssymbol = $stock->symbol;
+                    //                        echo "<br><br>";
+
+                    //                        print "Stock Symbol is:  $ssymbol";
+
+                    $URL = "http://www.google.com/finance/info?q=NSE:" . $ssymbol;
+                    $file = fopen("$URL", "r");
+                    $r = "";
+                    do {
+                        $data = fread($file, 500);
+                        $r .= $data;
+                    } while (strlen($data) != 0);
+                    //Remove CR's from ouput - make it one line
+                    $json = str_replace("\n", "", $r);
+
+                    //Remove //, [ and ] to build qualified string
+                    $data = substr($json, 4, strlen($json) - 5);
+
+                    //decode JSON data
+                    $json_output = json_decode($data, true);
+                    //echo $sstring, "<br>   ";
+                    $price = "\n" . $json_output['l'];
+                    //                        var_dump(json_decode($data));
+                    //                        var_dump($price);
+                    //echo "<br><br>";
+                    //                        echo "Price in dollars delayed by 20 minutes $ ";
+                                            echo $price;
+                    //$stockprice=$price;
+
+
+                    ?>
+                </td>
+            </tr>
             </tbody>
         </table>
     </div>
